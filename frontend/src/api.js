45ef321ef25784ad6daad558f27bee12cefc8d14
tejaswibathina <tejaswibathina2004@gmail.com>
2026-06-20 16:28:46 +0530

    Fixed duplicate files and build issues

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,18 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://hospital-admin-1-t3j6.onrender.com";
-
 const api = axios.create({
-  baseURL: API_BASE_URL,
-});
-
-export default api;
-import axios from "axios";
-
-const API_BASE_URL = "https://hospital-admin-1-t3j6.onrender.com";
-
-const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: "http://localhost:8000",
 });
 
 export default api;
